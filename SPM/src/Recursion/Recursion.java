4f950db5782a4b2d435a69132f22b63101aaf22e
Hashini Warnakulasooriya <hash_hw@icloud.com>
2019-08-16 13:08:19 +0530

    eigth commit from Hashini

--- SPM/src/Recursion/Recursion.java
+++ SPM/src/Recursion/Recursion.java
@@ -7,10 +7,11 @@ import java.util.Iterator;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.junit.*;
 
 public class Recursion {
 
-    private static int recCount = 0;
+    private static int Cs = 0;
     private static boolean isInheritance = false;
     static String path = "C:\\Users\\hwarlk\\Desktop\\sampleText.txt";
 
@@ -22,40 +23,49 @@ public class Recursion {
         Scanner sc = new Scanner(file);
         new Recursion().ReadFromFile(sc);
 
-        System.out.println("The value of Cs = " + recCount);
+        System.out.println("The value of Cs = " + Cs);
         System.out.println("Inheritance = " + isInheritance);
     }
-    
+
+    /**
+     *
+     * @param sc
+     * @throws IOException
+     */
     private void ReadFromFile(Scanner sc) throws IOException {
 
-        String outerMethodName = "";
-        String innerMethodName = "";
+        String outerMethodName = "outer";
+        String innerMethodName = "inner";
 
         HashMap<String, Integer> outerMethodNameArray = new HashMap<String, Integer>();
         HashMap<String, Integer> innerMethodNameArray = new HashMap<String, Integer>();
         int count = 0;
-        
         while (sc.hasNextLine()) {
+
             count++;
             String statement = sc.nextLine();
-            
-            if (checkColon(statement) == false) {
+//            System.out.println(statement);
+//            System.out.println(Pattern.matches("(//.*?$)|(/\\\\*.*?\\\\*/)", statement));
+            if (!Pattern.matches("(//.*?$)|(/\\\\*.*?\\\\*/)", statement)) {
+                
+                if (checkColon(statement) == false) {
 
-                if (checkOpenBrace(statement) == true) {
+                    if (checkOpenBrace(statement) == true) {
 
-                    outerMethodName = getMethodName(statement);
+                        outerMethodName = getMethodName(statement);
 
-                    outerMethodNameArray.put(outerMethodName, count);
+                        outerMethodNameArray.put(outerMethodName, count);
 
-                }
-            } else if (checkColon(statement) == true) {
+                    }
+                } else if (checkColon(statement) == true) {
 
-                if (checkOpenBrace(statement) == true) {
+                    if (checkOpenBrace(statement) == true) {
 
-                    innerMethodName = getMethodName(statement);
+                        innerMethodName = getMethodName(statement);
 
-                    innerMethodNameArray.put(innerMethodName, count);
+                        innerMethodNameArray.put(innerMethodName, count);
 
+                    }
                 }
             }
 
@@ -64,29 +74,36 @@ public class Recursion {
                 isInheritance = true;
             }
         }
-        
         Iterator it = outerMethodNameArray.entrySet().iterator();
 
         while (it.hasNext()) {
 
-            HashMap.Entry pairOuter = (HashMap.Entry) it.next();
+            HashMap.Entry pair = (HashMap.Entry) it.next();
 
             Iterator itInner = innerMethodNameArray.entrySet().iterator();//change innerMethodNameArray 
             while (itInner.hasNext()) {
+
                 HashMap.Entry pairInner = (HashMap.Entry) itInner.next();
-//                System.out.println(pairOuter.getKey() + " --------- " + pairInner.getKey());
-                String innerKey = pairOuter.getKey().toString();
+//                System.out.println(pair.getKey() + " --------- " + pairInner.getKey());
+                String innerKey = pair.getKey().toString();
                 String outerKey = pairInner.getKey().toString();
                 if (innerKey.equals(outerKey)) {
-                  it.remove();
-                  itInner.remove();
 
-                  recCount++;
+                    it.remove();
+                    itInner.remove();
+
+                    Cs++;
                 }
             }
+
         }
     }
 
+    /**
+     *
+     * @param statement
+     * @return
+     */
     private String getMethodName(String statement) {
 
         String[] name = statement.split("\\(");
@@ -97,7 +114,7 @@ public class Recursion {
 
                 String[] arrWords = name[i].split(" ");
                 methodName = arrWords[arrWords.length - 1];
-                System.out.println("-->" + methodName);
+//                System.out.println("-->" + methodName);
                 break;
             }
         }
@@ -105,6 +122,11 @@ public class Recursion {
         return methodName;
     }
 
+    /**
+     *
+     * @param statement
+     * @return
+     */
     private boolean checkOpenBrace(String statement) {
 
         if (statement.contains("(")) {
@@ -114,6 +136,24 @@ public class Recursion {
         return false;
     }
 
+    /**
+     *
+     * @param statement
+     * @return
+     */
+    private boolean checkCloseBrace(String statement) {
+
+        if (statement.contains(")")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param statement
+     * @return
+     */
     private boolean checkColon(String statement) {
 
         if (statement.contains(";")) {
@@ -122,17 +162,32 @@ public class Recursion {
         return false;
     }
 
-    private boolean checkInheritance(String statement) {
+    /**
+     *
+     * @param statement
+     * @return
+     */
+    public boolean checkInheritance(String statement) {
 
         String[] classCode = statement.split(" ");
 
         for (String name : classCode) {
 
             if (name.equals("implements") || name.equals("extends")) {
-               
+
                 return true;
             }
         }
         return false;
+    }
+    
+    @Test
+    public void testCheckInheritance(){
+        
+        String lineName = "class";
+        boolean expresult = true; 
+        Recursion re = new Recursion();
+        boolean result = re.checkInheritance(lineName);
+        Assert.assertEquals(expresult, result);
     }
 }
